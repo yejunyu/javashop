37863b8bddceca0c5bb63b2ed23c57a5f77f35da
yy <liuxing_yue@126.com>
2018-03-15 18:03:05 +0800

    支付宝接口

--- src/main/java/com/mmall/controller/portal/OrderController.java
+++ src/main/java/com/mmall/controller/portal/OrderController.java
@@ -1,18 +1,9 @@
 package com.mmall.controller.portal;
 
-import com.mmall.common.Const;
-import com.mmall.common.ResponseCode;
-import com.mmall.common.ServerResponse;
-import com.mmall.pojo.User;
-import com.mmall.service.IOrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpSession;
 
 /**
  * @author yejunyu
@@ -24,15 +15,15 @@ public class OrderController {
 
     private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
 
-    @Autowired
-    private IOrderService iOrderService;
+//    @Autowired
+//    private IOrderService iOrderService;
 
-    @RequestMapping("create.do")
-    @ResponseBody
-    public ServerResponse create(HttpSession session, Integer shippingId) {
-        User user = (User) session.getAttribute(Const.CURRENT_USER);
-        if (null == user) {
-            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
-        }
-    }
+//    @RequestMapping("create.do")
+//    @ResponseBody
+//    public ServerResponse create(HttpSession session, Integer shippingId) {
+//        User user = (User) session.getAttribute(Const.CURRENT_USER);
+//        if (null == user) {
+//            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+//        }
+//    }
 }
--- src/main/java/com/mmall/service/IOrderService.java
+++ src/main/java/com/mmall/service/IOrderService.java
@@ -1,12 +1,10 @@
 package com.mmall.service;
 
-import com.mmall.common.ServerResponse;
-
 /**
  * @author yejunyu
  * @date 18-3-12.
  */
 public interface IOrderService {
 
-    ServerResponse createOrder(Integer userId, Integer shippingId);
+//    ServerResponse createOrder(Integer userId, Integer shippingId);
 }
