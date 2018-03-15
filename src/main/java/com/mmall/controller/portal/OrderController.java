1f75fc325c2a1928c1109bbd79d9724447f11919
yejunyu <liuxing_yue@126.com>
2018-03-16 00:22:03 +0800

    二维码支付封装

--- src/main/java/com/mmall/controller/portal/OrderController.java
+++ src/main/java/com/mmall/controller/portal/OrderController.java
@@ -1,9 +1,20 @@
 package com.mmall.controller.portal;
 
+import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IOrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * @author yejunyu
@@ -15,15 +26,33 @@ public class OrderController {
 
     private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
 
-//    @Autowired
-//    private IOrderService iOrderService;
+    @Autowired
+    private IOrderService iOrderService;
 
-//    @RequestMapping("create.do")
-//    @ResponseBody
-//    public ServerResponse create(HttpSession session, Integer shippingId) {
-//        User user = (User) session.getAttribute(Const.CURRENT_USER);
-//        if (null == user) {
-//            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
-//        }
-//    }
+    /**
+     * 二维码支付
+     * @param session
+     * @param orderNo
+     * @param request
+     * @return
+     */
+    @RequestMapping("pay.do")
+    @ResponseBody
+    public ServerResponse pay(HttpSession session, Long orderNo, HttpServletRequest request){
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (null == user) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        String path = request.getSession().getServletContext().getRealPath("upload");
+        return iOrderService.pay(orderNo, user.getId(), path);
+    }
+
+    @RequestMapping("create.do")
+    @ResponseBody
+    public ServerResponse create(HttpSession session, Integer shippingId) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (null == user) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+    }
 }
--- src/main/java/com/mmall/service/IOrderService.java
+++ src/main/java/com/mmall/service/IOrderService.java
@@ -1,10 +1,14 @@
 package com.mmall.service;
 
+import com.mmall.common.ServerResponse;
+
 /**
  * @author yejunyu
  * @date 18-3-12.
  */
 public interface IOrderService {
 
-//    ServerResponse createOrder(Integer userId, Integer shippingId);
+    ServerResponse pay(Long orderNo,Integer userId, String path);
+
+    ServerResponse createOrder(Integer userId, Integer shippingId);
 }
